0a44bed99a69519d4bba095edc95cff158b4b296
Drake Rentrop <drakerentrop@gmail.com>
2018-11-14 20:20:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.each do |calls|
     loud_calls << "#{calls.upcase}!"
   end
-  loud_calls(planeteer_calls)
+  summon_captain_planet
     # code an argument here
   # Your code here
 end
